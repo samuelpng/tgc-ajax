421363afd372f41a8d41c2e32b495936dd1451fa
Samuel Png <samuelpng7@gmail.com>
2022-05-26 15:14:48 +0000

    q1,2,4,5

--- hands-on/q2-read-file-as-items/script.js
+++ hands-on/q2-read-file-as-items/script.js
@@ -1,5 +1,18 @@
-async function run() {
-    let response = await axios.get('items.txt')
-    console.log(response)
+btn = document.querySelector('#load-btn')
+btn.addEventListener('click', async function(){
+  let response = await axios.get('items.txt');
+  console.log(response.data)
+  fruitArray = response.data.split(",")
+  for (i of fruitArray){
+    fruitsList = document.querySelector('ul#fruits-list');
+    console.log(i)
+    //fruitsList.innerHTML = `<li>${i}</li>`
+    let liElement = document.createElement('li');
+    liElement.innerHTML = i
+    fruitsList.appendChild(liElement);
   }
-run()
+})
+
+
+
+
